8aae892bcc461923ad9ba68ea3dc717449850e6f
migueref <reafmiguel@gmail.com>
2018-02-23 14:26:39 -0600

    Relationships for Question model

--- app/Question.php
+++ app/Question.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function options()
+    {
+      return $this->belongsToMany('App\Option');
+    }
+    public function exams()
+    {
+      return $this->belongsToMany('App\Exam');
+    }
+    public function answer()
+    {
+      return $this->hasOne('App\Answer');
+    }
 }
