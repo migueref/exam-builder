be5027f6c06eab75f4e40af18e8181b1f842eaad
migueref <reafmiguel@gmail.com>
2018-02-23 14:33:05 -0600

    Relationships for Answer model

--- app/Answer.php
+++ app/Answer.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    //
+    public function question()
+    {
+        return $this->belongsTo('App\Question');
+    }
+    public function option()
+    {
+       return $this->belongsTo('App\Option');
+    }
+    public function form()
+    {
+       return $this->belongsTo('App\Form');
+    }
 }
