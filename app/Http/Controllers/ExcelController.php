f7fb0fdb19fad9290244b4a11d93d3ad172c363b
migueref <reafmiguel@gmail.com>
2018-02-27 14:48:53 -0600

    Excel report for tests

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -4,40 +4,50 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Form;
+use App\Exam;
 
 class ExcelController extends Controller
 {
+
   public function exportForms($id)
   {
-    \Excel::create('formacion-continua-reporte', function($excel) {
-      $forms = Form::all();
-      $excel->sheet('Excel sheet', function($sheet) use($forms) {
-        $sheet->row(1, [
-          '1. Su principal interés para estudiar un doctorado en CIFE es:',
-          '2. Mi desempeño académico en la maestría fue:',
-          '3. Mi grado de responsabilidad para terminar trabajos y actividades académicas y profesionales en tiempo y forma, es:',
-          '4. En el futuro, espero que con este doctorado pueda (señale la opción más valiosa para Usted):',
-          '5. Si ingreso al doctorado, probablemente:',
-          '6. Si ingreso al doctorado, probablemente:',
-          '7. Deseo realizar un proceso de titulación en el doctorado con énfasis en: ',
-          '8. Me interesa estudiar en el doctorado:',
-          '9. Respecto a la redacción de un artículo, mi desempeño es:',
-          '10. Mi desempeño en cursos virtuales o mediados por la tecnología es:',
-          '11. EVALUACIÓN DEL DESEMPEÑO EN TORNO A LA REDACCIÓN DE TEXTOS',
-          '12. Resumen de un texto',
-          '13. Señale a continuación una idea que no se aborde en la Introducción. ',
-          'Fecha de registro',
-
-        ]);
-        foreach($forms as $index => $form) {
-            $sheet->row($index+2, [
-              $form->firstname
-              ,$form->middlename
-              ,$form->lastname
-              ,$form->city
-            ]);
-          }
-      });
-    })->export('xls');
+      $registers = $this->getFormAnswers($id);
+      $questions = $this->getExamQuestions($id);
+      \Excel::create('Registros-examen', function($excel) use($registers,$questions) {
+          $excel->sheet('Sheetname', function($sheet) use($registers,$questions) {
+              $sheet->fromArray($registers,null,'A2',false,false)->prependRow($questions);
+          });
+      })->export('xls');
   }
+
+  public function getFormAnswers($id)
+  {
+      $forms = Form::where('exam_id',$id)->get();
+      $registers =array();
+      foreach($forms as $form) {
+          $data = array();
+          foreach($form->answers as $answer) {
+              if ($answer->question->type == 1) {
+                $option = $answer->option["name"];
+                array_push($data, $option);
+              } else {
+
+                array_push($data, strip_tags($answer->description));
+              }
+          }
+          array_push($registers, $data);
+      }
+      return $registers;
+  }
+
+  public function getExamQuestions($id)
+  {
+      $exam = Exam::find($id);
+      $questions = array();
+      foreach ($exam->questions as $question) {
+        array_push($questions, strip_tags($question->name));
+      }
+      return $questions;
+  }
+
 }
